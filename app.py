2822c1b7d8e4b0e8723bd3770eda4de00d2b2178
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-06-07 04:07:58 -0400

    app

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def create_app(test_config=None):
           }), 400
     return app
 
-APP = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
